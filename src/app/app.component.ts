03c29ba2f41353abd25c01aba0e5a74c07357d31
LDprg <71488985+LDprg@users.noreply.github.com>
2023-02-05 15:22:20 +0100

    Add statistics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from "@angular/router";
+import { filter } from "rxjs";
+
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'LDprg';
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.setUpAnalytics();
+  }
+
+  setUpAnalytics() {
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        gtag("config", "G-55C8CP38WR",
+          {
+            page_path: event.urlAfterRedirects
+          }
+        );
+      });
+  }
 }
